1d28493e62e7fb7d3e9fd5a2103ea047a8cf3a98
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-08 23:54:10 -0400

    feat(turing): simulates a turing machine

--- turing.py
+++ turing.py
@@ -12,8 +12,30 @@
     # B, s4 -> B and move head back to the left , s1
 
 X_B = {
-    ("B", "s1"): ("X", "R" "s2"),
-    ("B", "s2"): ("B", "L" "s3"),
-    ("X", "s3"): ("B", "R" "s4"),
-    ("B", "s4"): ("B", "L" "s1")
+    ("B", "s1"): ("X", "R", "s2"),
+    ("B", "s2"): ("B", "L", "s3"),
+    ("X", "s3"): ("B", "R", "s4"),
+    ("B", "s4"): ("B", "L", "s1")
 }
+
+def simulate(instructions):
+  # set up initial state (ideally not hardcoded like below)
+  # tape
+  tape = ["B", "B"]
+  # head
+  head = 0
+  # state
+  state = "s1"
+
+  # loop:
+  for _ in range(8):
+    print(tape)
+  # look up instruction
+    key = (tape[head], state)
+    tape_sym, head_dir, new_state = instructions[key]
+  # apply that instruction to the machine
+    tape[head] = tape_sym
+    head += 1 if head_dir == "R" else - 1
+    state = new_state
+
+simulate(X_B)
