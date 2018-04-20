fde385feb0db9ba8f70a2d4a8c33843e5676b729
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-19 21:51:38 -0400

    converted to lambda except assignment

--- lambda.py
+++ lambda.py
@@ -1,8 +1,27 @@
-IF = lambda cond: lambda t_func: lambda f_func: t_func(None) if cond else f_func(None)
-IS_ZERO = lambda x: x == 0
-ONE = 1
-MULT = lambda x: lambda y: x * y
-SUB1 = lambda x: x - 1
+# Church numerals -> identity -> add -> multiply
+ZERO = lambda f: lambda x: x
+# ONE = lambda f: lambda x: f(x)
+# IDENTITY = lambda n: (lambda f: lambda x: n(f)(x))
+ADD1 = lambda n: (lambda f: lambda x: f(n(f)(x)))
+ADD = lambda n: lambda m: n(ADD1)(m)
+MULT = lambda n: lambda m: n(lambda acc: ADD(m)(acc))(ZERO)
+
+SUB1 = (
+  lambda n:
+  lambda f:
+  lambda x: n (lambda g: lambda h: h(g(f)))
+              (lambda u: x)
+              (lambda u: u)
+)
+
+NULL = lambda x: x
+TRUE = lambda t: lambda f: t(NULL)
+FALSE = lambda t: lambda f: f(NULL)
+IF = lambda cond: lambda t: lambda f: cond(t)(f)
+IS_ZERO = lambda n: n(lambda x: FALSE)(TRUE)
+
+ONE = ADD1(ZERO)
+SIX = ADD1(ADD1(ADD1(ADD1(ADD1(ONE)))))
 
 print(
   (
@@ -11,9 +30,9 @@ print(
         IF(
           IS_ZERO(n)
         )(
-          lambda _: ONE,
+          lambda _: ONE
         )(
-          lambda _: MULT(n)(myself(myself)(SUB1(n)))
+          lambda _: MULT(n)( myself(myself)(SUB1(n)) )
         )
       )
     )
@@ -30,5 +49,7 @@ print(
       )
     )
   )
-  (6)
+  (SIX)
+  # the following line just converts the lambda function to a python number
+  (lambda x: x + 1)(0)
 )
