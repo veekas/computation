131cd9dcdff83b43f926406f6277d1e453c7a81d
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-19 20:48:58 -0400

    add factorial function to convert to lambda

---
+++ lambda.py
@@ -0,0 +1,13 @@
+def fact(n):
+  if n == 0:
+    return 1
+  else:
+    return n * fact(n - 1)
+
+print(fact(0))
+print(fact(1))
+print(fact(2))
+print(fact(3))
+print(fact(4))
+print(fact(5))
+print(fact(6))
