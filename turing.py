5be7c047f018d8f55d0fd9542cd6af37ef680799
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-19 20:40:19 -0400

    create adder function in turing machine

--- turing.py
+++ turing.py
@@ -15,12 +15,12 @@ X_B = {
 
 def simulate(instructions):
   # set up initial state (ideally not hardcoded like below)
-  tape = ["B", "B"]
+  tape = ["B"] * 24
   head = 0
   state = "s1"
 
   # loop:
-  for _ in range(8):
+  for _ in range(24):
     # print statements just show results in console, not necessary for machine
     print(state.rjust(2) + ": " + "".join(tape))
     print("    " + " " * head + "^")
@@ -30,3 +30,37 @@ def simulate(instructions):
     head += 1 if head_dir == "R" else - 1
 
 simulate(X_B)
+
+# next step: show that we can do the following
+# repetition, consitionals, compound nested data (DS in DS)
+
+# use unary numbers (1 = 1, 2 = 11, 3 = 111, ...)
+ADDER = {
+  # (11+111)BBBBBBBBBBB
+  #         ^
+
+  ("B", "s1"): ("(", "R", "s2"),
+  ("B", "s2"): ("1", "R", "s3"),
+  ("B", "s3"): ("1", "R", "s4"),
+  ("B", "s4"): ("+", "R", "s5"),
+  ("B", "s5"): ("1", "R", "s6"),
+  ("B", "s6"): ("1", "R", "s7"),
+
+  # ("B", "s7"): ("1", "R", "s8"),
+  ("B", "s7"): ("1", "R", "s7b"),
+  ("B", "s7b"): ("1", "R", "s8"),
+
+  ("B", "s8"): (")", "R", "s9"),
+
+  ("B", "s9"): ("B", "L", "s9"),
+  (")", "s9"): (")", "L", "s9"),
+  ("1", "s9"): ("1", "L", "s9"),
+
+  ("+", "s9"): ("1", "R", "s10"),
+  ("1", "s10"): ("1", "R", "s10"),
+  (")", "s10"): ("B", "L", "s11"),
+  ("1", "s11"): (")", "R", "s12"),
+  ("B", "s12"): ("B", "R", "s12"),
+}
+
+simulate(ADDER)
