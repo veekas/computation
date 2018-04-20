68592516cf8181dc61737adc75a8428ad480dfc1
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-19 22:52:13 -0400

    holy moly it's done

--- lambda.py
+++ lambda.py
@@ -1,55 +1,136 @@
-# Church numerals -> identity -> add -> multiply
-ZERO = lambda f: lambda x: x
-# ONE = lambda f: lambda x: f(x)
-# IDENTITY = lambda n: (lambda f: lambda x: n(f)(x))
-ADD1 = lambda n: (lambda f: lambda x: f(n(f)(x)))
-ADD = lambda n: lambda m: n(ADD1)(m)
-MULT = lambda n: lambda m: n(lambda acc: ADD(m)(acc))(ZERO)
-
-SUB1 = (
-  lambda n:
-  lambda f:
-  lambda x: n (lambda g: lambda h: h(g(f)))
-              (lambda u: x)
-              (lambda u: u)
-)
-
-NULL = lambda x: x
-TRUE = lambda t: lambda f: t(NULL)
-FALSE = lambda t: lambda f: f(NULL)
-IF = lambda cond: lambda t: lambda f: cond(t)(f)
-IS_ZERO = lambda n: n(lambda x: FALSE)(TRUE)
-
-ONE = ADD1(ZERO)
-SIX = ADD1(ADD1(ADD1(ADD1(ADD1(ONE)))))
-
 print(
   (
     lambda myself: (
       lambda n: (
-        IF(
-          IS_ZERO(n)
+        (
+          lambda cond: lambda t: lambda f: cond(t)(f)
         )(
-          lambda _: ONE
+          (
+            lambda n: n(
+              lambda x: (
+                lambda t: lambda f: f(lambda x: x)
+              )
+            )
+            (lambda t: lambda f: t(lambda x: x))
+          )
+          (n)
         )(
-          lambda _: MULT(n)( myself(myself)(SUB1(n)) )
+          lambda _: (
+            lambda n: (
+              lambda f: lambda x: f(n(f)(x))
+            )
+          )
+          (lambda f: lambda x: x)
+        )(
+          lambda _: (
+            lambda n: lambda m: n(
+              lambda acc: (
+                  lambda n: lambda m: n(
+                    lambda n: lambda f: lambda x: f(n(f)(x))
+                  )(m)
+              )
+              (m)
+              (acc)
+            )
+            (lambda f: lambda x: x)
+          )
+          (n)
+          (
+            myself(
+              myself
+            )(
+              (
+                lambda n:
+                lambda f:
+                lambda x: n(lambda g: lambda h: h(g(f)))
+                (lambda u: x)
+                (lambda u: u)
+              )
+              (n)
+            )
+          )
         )
       )
     )
   )(
     lambda myself: (
       lambda n: (
-        IF(
-          IS_ZERO(n)
+        (
+          lambda cond: lambda t: lambda f: cond(t)(f)
         )(
-          lambda _: ONE,
+            lambda n: n(
+              lambda x: (
+                lambda t: lambda f: f(lambda x: x)
+              )
+            )
+            (lambda t: lambda f: t(lambda x: x))
+          )
+          (n)
+        (
+          lambda _: (
+              lambda n: (
+                  lambda f: lambda x: f(n(f)(x))
+              )
+          )
+          (lambda f: lambda x: x)
         )(
-          lambda _: MULT(n)(myself(myself)(SUB1(n)))
+          lambda _: (
+            lambda n: lambda m: n(
+              lambda acc: (
+                lambda n: lambda m: n(
+                  lambda n: lambda f: lambda x: f(n(f)(x))
+                )(m)
+              )
+              (m)
+              (acc)
+            )
+            (lambda f: lambda x: x)
+          )
+          (n)
+          (
+            myself(
+              myself
+            )(
+              (
+                lambda n:
+                lambda f:
+                lambda x: n(lambda g: lambda h: h(g(f)))
+                (lambda u: x)
+                (lambda u: u)
+              )
+              (n)
+            )
+          )
         )
       )
     )
-  )
-  (SIX)
+  )((
+      (
+        lambda n: lambda f: lambda x: f(n(f)(x))
+      )(
+        (
+          lambda n: lambda f: lambda x: f(n(f)(x))
+        )(
+          (
+            lambda n: lambda f: lambda x: f(n(f)(x))
+          )(
+            (
+              lambda n: lambda f: lambda x: f(n(f)(x))
+            )(
+              (
+                lambda n: lambda f: lambda x: f(n(f)(x))
+              )(
+                (
+                  lambda n: lambda f: lambda x: f(n(f)(x))
+                )(
+                  (lambda f: lambda x: x)
+                )
+              )
+            )
+          )
+        )
+      )
+  ))
   # the following line just converts the lambda function to a python number
   (lambda x: x + 1)(0)
 )
